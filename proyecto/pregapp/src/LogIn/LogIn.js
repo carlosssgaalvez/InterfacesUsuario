464beeb475bf6e332cf1c54d33bfc52100e8f5a6
IvanPD2002 <100696419+IvanPD2002@users.noreply.github.com>
2025-04-03 13:42:28 +0200

    textlinkbutton deleted

--- proyecto/pregapp/src/LogIn/LogIn.js
+++ proyecto/pregapp/src/LogIn/LogIn.js
@@ -5,10 +5,12 @@ import Image from '../components/Image/Image';
 import Logo from '../images/logo.png';
 import Title from '../components/Text/Title';
 import PlainText from '../components/Text/PlainText';
-import TextLink from './components/TextLinkButton';
+import { useState } from 'react';
+import Form from '../components/Form/Form';
 
 function LogIn(){
-
+  // Esto es un ejemplo para probar los componentes
+   
   const handleClick = () => {
     console.log("Button clicked!");
   }
@@ -16,15 +18,23 @@ function LogIn(){
   const handleClickTextLink = () => {
     console.log("TextLink clicked!");
   }
+    
+  const [inputValue, setInputValue] = useState(''); 
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    return (
+  return (
     <div className="LogIn">
       <header className="LogIn-header">   
         <Image className={"imgLogo"} src={Logo}/>
         <Title className="title" valueText={'Titulo'}/>
         <Button className="buttonSalir" valueButton={'SALIR'} onClick={handleClick}/>
         <PlainText className="text" textValue={'Texto de ejemplo'}/>
-        <TextLink className="textLink" textValue={'Texto enlace de ejemplo'} onClick={handleClickTextLink}/>
+        <Button className="textLink" valueButton={'Texto enlace de ejemplo'} onClick={handleClickTextLink}/>
+        <br/>
+        <Form className={"form"} placeholder={"Escribe algo"} value={inputValue} onChange={handleChange}/>
+        <PlainText className="text" textValue={inputValue}/>
       </header>
     </div>
     );
--- proyecto/pregapp/src/LogIn/components/TextLinkButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TextLinkButton({ className, textValue, onClick}) {
-    return (<button className={className} onClick={onClick}>{textValue}</button>);
-  }
-  
-export default TextLinkButton;
