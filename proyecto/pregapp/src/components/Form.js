973cf254be0f72c258574a2daa18434d1f76d827
GregorioMerchan <gregomerchan@uma.es>
2025-04-03 12:27:32 +0200

    Update Form.js

--- proyecto/pregapp/src/components/Form.js
+++ proyecto/pregapp/src/components/Form.js
@@ -1,25 +1,6 @@
-function Form({ label, placeholder, onChange }) {
-    const [value, setValue] = useState("");
-  
-    function handleChange(e) {
-      setValue(e.target.value);
-      if (onChange) {
-        onChange(e.target.value);
-      }
-    }
-  
-    return (
-      <div className="flex flex-col space-y-2">
-        {label && <label className="text-gray-700 font-medium">{label}</label>}
-        <input
-          type="text"
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-          className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-    );
-  }
-  
-  export default Form;
+function Form({ className, placeholder, value, onChange }) {
+  return ( <input type="text" placeholder={placeholder} className={className} onChange={onChange} value={value}  />
+  );
+}
+
+export default Form;
