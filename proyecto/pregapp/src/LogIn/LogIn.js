1a0fc92a895e570b983831f860a69a16c6815186
IvanPD2002 <100696419+IvanPD2002@users.noreply.github.com>
2025-04-03 10:48:06 +0200

    src update

---
+++ proyecto/pregapp/src/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+import './LogIn.css';
+
+function LogIn(){
+    return (
+    <div className="LogIn">
+      <header className="LogIn-header">   
+      </header>
+    </div>
+    );
+}
+
+export default LogIn;
