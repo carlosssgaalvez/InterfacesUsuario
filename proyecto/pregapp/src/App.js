b03d45a3290093cf8a0af05c481e95cc50fd0e83
IvanPD2002 <100696419+IvanPD2002@users.noreply.github.com>
2025-04-08 13:21:16 +0200

    HOME

--- proyecto/pregapp/src/App.js
+++ proyecto/pregapp/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './pages/home/Home';
 import LogIn from "./pages/logIn/LogIn.js";
 import Register from './pages/register/Register.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,7 +12,11 @@ function App() {
           <Router>
             <Routes>
               <Route path="/login" element={<LogIn />} />       {/* Página principal */}
+<<<<<<< Updated upstream
               <Route path="/register" element={<Register />} /> {/* Página de registro */}
+=======
+              <Route path="/home" element={<Home />} />       {/* Página principal */}
+>>>>>>> Stashed changes
             </Routes>
           </Router>
     </div>
---
+++ proyecto/pregapp/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../../components/Text/Title';
+
+function Home() {
+  return (
+    <div>
+        <Title className={'title'} valueText={'nombreJuego'}/>
+    </div>
+  );
+}
+
+export default Home;
