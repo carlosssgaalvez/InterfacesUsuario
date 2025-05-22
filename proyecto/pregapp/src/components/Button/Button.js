60599bc08c998278264d5846658d3bb3580ed533
carlosssgaalvez <carlossgaalvez@uma.es>
2025-05-22 09:28:14 +0200

    Button general adaptado para ciegos

--- proyecto/pregapp/src/components/Button/Button.js
+++ proyecto/pregapp/src/components/Button/Button.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { speakIfTabbing } from '../../utils/speech';
 
 function Button({idButton, className, valueButton, onClick}) {
+    const handleFocus = () => {
+            speakIfTabbing(valueButton);
+    };
+
     return (
-        <button id={idButton} className={className} onClick={onClick}>{valueButton}</button>
+        <button id={idButton} className={className} onClick={onClick} onFocus={handleFocus}>{valueButton}</button>
     );
 }
 
